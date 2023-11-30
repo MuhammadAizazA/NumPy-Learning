1ba92a05f9185e4a6d7667f63c173ecf3c710f41
MuhammadAizazA <aizaz.ali.shah.0205@gamil.com>
2023-11-30 20:18:23 +0500

    practiced numpy

--- src/module_1.py
+++ src/module_1.py
@@ -2,4 +2,8 @@ def check_package_working():
     print("Package is working")
     
 if __name__=='__main__':
-    check_package_working()
+    import numpy as np
+
+    N, M = list(map(int, input().split()))
+
+    print(N, M)
